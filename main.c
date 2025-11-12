2ccedb800a28dc7f9baf0f96dad4ef3f9747cc20
Kheldin <kheldin.acherchour@gmail.com>
2025-11-12 11:23:07 +0100

    maiin test

--- main.c
+++ main.c
@@ -5,17 +5,8 @@ int main (void)
 {
     int a = 0;
 
-    ft_printf("Pas d'args ici\n", 12);
-    ft_printf("%ubonjour je %d\n", -12, 45);
-    ft_printf("%s carac : %c\n", "Je suis une str", 'z');
-    ft_printf("%x\n", 39);
-    ft_printf("%x\n", 16);
-    ft_printf("%x\n", 32);
-    ft_printf("%x\n", 0);
-    printf("%x\n", 39);
-    printf("%x\n", 16);
-    printf("%x\n", 32);
-    printf("%x\n", 0);
+    ft_printf("%c %s %d %i %p %x %X\n", 'Q', "bjr", 1, 2, &a, 78, 78);
+    printf("%c %s %d %i %p %x %X\n", 'Q', "bjr", 1, 2, &a, 78, 78);
     ft_printf("%%%%\n"); // Should print 2 % instead of 3
     printf("%%%%\n");
     ft_printf("%p\n", &a);
